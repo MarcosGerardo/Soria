6254765d5ff2be8b4a966ca76f905e5960c2191f
MarcosGerardo <19040368@itdurango.edu.mx>
2022-11-07 07:18:06 -0600

    primer intento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbaroneComponent } from './navbarone/navbarone.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CardComponent } from './card/card.component';
+import { NavbartwoComponent } from './navbartwo/navbartwo.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbaroneComponent,
+    CarouselComponent,
+    CardComponent,
+    NavbartwoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
